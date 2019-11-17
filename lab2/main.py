ef63b30f61fcdd252122603b2ec7ba340bd2f483
Max Farafonov <farafonov.max@gmail.com>
2019-11-18 01:11:03 +0300

    Complete the first part of the second lab

--- lab2/main.py
+++ lab2/main.py
@@ -1,12 +1,31 @@
+import math
+
 import pandas as pd
 import numpy as np
 
 
-def create_design_matrix(*xs):
-    ones = pd.Series(1, index=np.arange(len(xs[0])))
-    result = pd.concat([ones, *xs], axis="columns")
-    result.columns = np.arange(len(result.columns))
-    return result
+def create_design_matrix(x, normalize=True):
+    ones = pd.Series(1, index=np.arange(len(x)))
+    new_x, means, widths = _normalize(x) if normalize else (x, 0, 1)
+    means = np.insert(means, 0, 0)
+    widths = np.insert(widths, 0, 1)
+    result = pd.concat(
+        [ones, new_x], axis="columns", names=(["dummy"].extend(x.columns))
+    )
+    return result, means, widths
+
+
+def denormalize_known(x, means, widths, index=None):
+    if index is None:
+        return widths * x + means
+    return widths[index] * x + means[index]
+
+
+def gradient_descent(theta, x, y, alpha):
+    gradient = lambda _theta: logistic_loss_gradient(_theta, x, y)
+    for current_theta in iterate_theta(gradient, theta, alpha):
+        loss = logistic_loss(current_theta, x, y)
+        yield current_theta, loss
 
 
 def iterate_theta(gradient, theta, alpha):
@@ -15,19 +34,36 @@ def iterate_theta(gradient, theta, alpha):
         theta = theta - alpha * gradient(theta)
 
 
+def logistic_cost(theta, x, y):
+    prediction = logistic_hypothesis(x, theta)
+    return -np.log(y * prediction + (1 - y) * (1 - prediction))
+
+
+def logistic_hypothesis(x, theta):
+    return sigmoid(np.dot(x, theta))
+
+
+def logistic_loss(theta, x, y):
+    return 1 / len(x) * logistic_cost(theta, x, y).sum()
+
+
 def logistic_loss_gradient(theta, x, y):
-    # NOTE: x and theta should be of the same size: assuming a row of 1's in x
-    len_x = len(x)
-    if len_x != len(y):
-        raise ValueError("Series must be of the same length")
-    sample_items = x.transpose() * (x.dot(theta) - y).values
-    return 1 / len_x * sample_items.apply(np.sum, axis="columns")
+    return (1 / len(x)) * np.dot(x.transpose(), logistic_hypothesis(x, theta) - y)
 
 
-def normalize(x):
-    def _normalize(row):
-        mean = row.mean()
-        width = row.max() - row.min()
-        return row.map(lambda item: (item - mean) / width)
+def normalize_known(x, means, widths, index=None):
+    if index is None:
+        return (x - means) / widths
+    return (x - means[index]) / widths[index]
 
-    return x.apply(_normalize, axis="rows")
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def _normalize(x):
+    means = [x[c].mean() for c in x.columns]
+    # TODO: consider width = 0
+    widths = [np.ptp(x[c]) for c in x.columns]
+    result = (x - means) / widths
+    return result, means, widths
