56b2ba9fda9d4871f621f5dff00668054bc7bdd7
Max Farafonov <farafonov.max@gmail.com>
2019-11-24 19:28:40 +0300

    Finish items 2 and 3 of the 5th lab

--- lab5/main.py
+++ lab5/main.py
@@ -1,2 +1,12 @@
 import numpy as np
-import pandas as np
+import pandas as pd
+from sklearn.svm import SVC
+from sklearn.metrics.pairwise import euclidean_distances
+
+
+def gaussian(sigma_squared):
+    def kernel(x, y):
+        distances = euclidean_distances(x, y)
+        return np.exp(-distances ** 2 / 2 / sigma_squared)
+
+    return kernel
