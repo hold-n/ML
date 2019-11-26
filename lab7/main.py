24b7c43f40f078458cedeb5437026126a61ac20d
Max Farafonov <farafonov.max@gmail.com>
2019-11-27 00:30:32 +0300

    Finish the first item of the 7th lab
    Implemented PCA for two-dimensional data. The number of dimensions is
    entered manually.

--- lab7/main.py
+++ lab7/main.py
@@ -1,2 +1,19 @@
 import numpy as np
 import pandas as pd
+
+
+def cov_matrix(x, is_scaled):
+    scaled = x if is_scaled else x - x.mean()
+    return 1 / len(x) * (scaled.T @ scaled)
+
+
+def pca(x, k, is_scaled=True):
+    c = cov_matrix(x, is_scaled)
+    # TODO: implement selection of k
+    u, _, _ = np.linalg.svd(c)
+    uk = u[:, :k]
+    return (x @ uk), uk.T
+
+
+def pca_inverse(x, uk):
+    return x @ uk
